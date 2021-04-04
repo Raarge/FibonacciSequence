826e1f8517694a5c270804e3063ab481aac931b6
Darin Campbell - Raarge Actual <animusdrogai@gmail.com>
2021-04-03 23:56:05 -0400

    Add project files.

---
+++ FibonacciSequence/CalculateFibonacci.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciSequence
+{
+    public static class CalculateFibonacci
+    {
+        public static int CalculatFibonacciAt(int numOfFibonacci)
+        {
+            if (numOfFibonacci < 2)
+                return numOfFibonacci;
+            else
+            {
+                return CalculatFibonacciAt(numOfFibonacci - 2) + CalculatFibonacciAt(numOfFibonacci - 1);
+            }
+        }
+    }
+}
---
+++ FibonacciSequence/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FibonacciSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(var i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(CalculateFibonacci.CalculatFibonacciAt(i));
+            }
+            Console.Read();
+        }
+    }
+}
